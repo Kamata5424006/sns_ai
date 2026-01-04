54333e63d05928c4db16b567ac6aebb5d6e0387c
keity <chke24006@g.nihon-u.ac.jp>
2026-01-05 01:23:04 +0900

    without DB

---
+++ components/PostForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import submit from "";
+
+export default function PostForm(){
+    const [text, setText] = useState("");
+    
+    // eはSyntheticEvent
+    function handlePost(e){
+        e.preventDefault(); // formのページ遷移を防止
+        // str.trim():先頭と末尾のホワイトスペースを取り除く
+        if(!text.trim()) return; // 空投稿防止
+        submit(text); 
+        setText("");
+    }
+    
+    return (
+        <div id="PostForm">
+            <div className="post_icon">
+                <img
+                    src="/img/user.png"
+                    alt="You"
+                    className="icon"
+                />
+            </div>
+
+            <div id="form_body">
+                <div className="input_frame">
+                    {/* onSubmit:送信時にJavaScriptを実行 */}
+                    <form onSubmit={handlePost}> 
+                        <textarea
+                            id="form_input"
+                            name="form_input"
+                            value={text}
+                            // valueが変化する毎レンダーし直す
+                            onChange={e => setText(e.target.value)}
+                            onKeyDown={e => {
+                                if(e.key === "Enter"){
+                                    e.preventDefault();
+                                }
+                            }}
+                        />
+                        <button type="submit" id="post">Post</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-
+import Header from "/componets/Header";
+import PostLists from "/components/PostLists";
+import PostForm from "/components/PostForm";
 
 export default function App(){
     return (
-        
-    )
+        <>
+            <Header />
+            <PostLists />
+            <PostForm />
+        </>
+    );
 }
