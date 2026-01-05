eedc912cc2c78cb3fad14c4d2599dca77d727632
keity <chke24006@g.nihon-u.ac.jp>
2026-01-05 23:51:44 +0900

    made database.db

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from "/componets/Header";
-import PostLists from "/components/PostLists";
-import PostForm from "/components/PostForm";
+import Header from "./componets/Header";
+import PostLists from "./components/PostLists";
+import PostForm from "./components/PostForm";
 
 export default function App(){
     return (
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+    <div id="header">
+        <p id="header_name">404:Humans Not Found</p>
+    </div>
+    );
+}
